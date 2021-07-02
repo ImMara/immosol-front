ad032b02e6be9e3f6cb5d7dd8e34d00b98865361
Mara <alaneraerts@live.be>
2021-07-02 10:19:42 +0200

    pagination

--- components/pagination/Pagination.jsx
+++ components/pagination/Pagination.jsx
@@ -14,16 +14,16 @@ const Pagination = (props) => {
     return (
         <>
             <ul className="pagination pagination-sm my-3 justify-content-center pagination-color">
-                <li className={"page-item" + (props.currentPage === 1 ? " disabled" : null)}>
-                    <button className="page-link" onClick={()=>props.onPageChanged(props.currentPage - 1)} >&laquo;</button>
+                <li className={"page-item rounded-start" + (props.currentPage === 1 && " disabled")}>
+                    <button className="page-link rounded-start" onClick={()=>props.onPageChanged(props.currentPage - 1)} >&laquo;</button>
                 </li>
                 {pages.map(page => (
-                    <li key={page} className={"page-item" + (props.currentPage === page ? " active" : null)}>
+                    <li key={page} className={"page-item" + (props.currentPage === page && " active")}>
                         <button className="page-link" onClick={() => props.onPageChanged(page)}>{page}</button>
                     </li>
                 ))}
-                <li className={"page-item" + (props.currentPage === pagesCount ? " disabled" : null)}>
-                    <button className="page-link" onClick={()=>props.onPageChanged(props.currentPage + 1)} >&raquo;</button>
+                <li className={"page-item rouded-end" + (props.currentPage === pagesCount && " disabled" )}>
+                    <button className="page-link rouded-end" onClick={()=>props.onPageChanged(props.currentPage + 1)} >&raquo;</button>
                 </li>
             </ul>
         </>
