62577689f2afd616b9b0d476f0f08a9a216074a8
Mara <alaneraerts@live.be>
2021-07-05 08:46:38 +0200

    change carou

--- components/carousel/Carousel.jsx
+++ components/carousel/Carousel.jsx
@@ -8,12 +8,10 @@ function Carousel(props) {
         <div id="carouselExampleIndicators" className="carousel slide" data-bs-ride="carousel">
 
             <div className="carousel-indicators">
-                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0"
-                        className="active" aria-current="true" aria-label="Slide 1"/>
-                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"
-                        aria-label="Slide 2"/>
-                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"
-                        aria-label="Slide 3"/>
+                {images.map((i,k)=>(
+                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to={k}
+                            className="active" aria-current="true" aria-label={k}/>
+                ))}
             </div>
 
             <div className="carousel-inner" style={{height:props.size}}>
